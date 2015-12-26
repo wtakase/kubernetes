1bc8aaa80d1dc2c43e55305e08d82334ba47b401
Wataru Takase <wataru.takase@kek.jp>
2015-12-26 13:10:20 +0900

    Fix variable name of pod name

--- cluster/addons/dns/kube2sky/kube2sky.go
+++ cluster/addons/dns/kube2sky/kube2sky.go
@@ -230,8 +230,8 @@ func (ks *kube2sky) handleEndpointAdd(obj interface{}) {
 func (ks *kube2sky) handlePodCreate(obj interface{}) {
 	if e, ok := obj.(*kapi.Pod); ok {
 		// If the pod ip is not yet available, do not attempt to create.
-		if e.Status.PodName != "" {
-			name := buildDNSNameString(ks.domain, podSubdomain, e.Namespace, e.Status.PodName)
+		if e.Name != "" {
+			name := buildDNSNameString(ks.domain, podSubdomain, e.Namespace, e.Name)
 			ks.mutateEtcdOrDie(func() error { return ks.generateRecordsForPod(name, e) })
 		}
 	}
@@ -256,8 +256,8 @@ func (ks *kube2sky) handlePodUpdate(old interface{}, new interface{}) {
 
 func (ks *kube2sky) handlePodDelete(obj interface{}) {
 	if e, ok := obj.(*kapi.Pod); ok {
-		if e.Status.PodName != "" {
-			name := buildDNSNameString(ks.domain, podSubdomain, e.Namespace, e.Status.PodName)
+		if e.Name != "" {
+			name := buildDNSNameString(ks.domain, podSubdomain, e.Namespace, e.Name)
 			ks.mutateEtcdOrDie(func() error { return ks.removeDNS(name) })
 		}
 	}
